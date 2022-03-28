9608aa2bf8d03b8bf11972e397a73208bfc2c726
Reema <reema.a.shadid@gmail.com>
2022-03-28 13:11:34 -0700

    Event and Location models completed and functional

--- Events/admin.py
+++ Events/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+class LocationInline(admin.StackedInline):
+    model = Location
+
+class EventAdmin(admin.ModelAdmin):
+    inlines = [
+        LocationInline,
+    ]
+
 admin.site.register(Event)
+admin.site.register(Location)
--- Events/models.py
+++ Events/models.py
@@ -3,18 +3,19 @@ from django.db import models
 
 # Create your models here.
 
-# class Location(models.Model):
-#     locationName= models.CharField(verbose_name='Business or Location Name', max_length=100)
-#     listing= models.URLField(verbose_name='Google Maps Listing')
-#     website= models.URLField(verbose_name='Website')
+class Location(models.Model):
+    locationName= models.CharField(verbose_name='Business or Location Name', max_length=100, null= False)
+    listing= models.URLField(verbose_name='Google Maps Listing', null= False)
+    website= models.URLField(verbose_name='Business or Location Website', null= True)
 
-    
+    def __str__(self):
+        return '{}'.format(self.locationName)
 
 class Event(models.Model):
 
-    #location= models.ForeignKey(Location, on_delete= models.CASCADE)
+    location= models.ForeignKey(Location, on_delete= models.CASCADE, null= False)
 
-    eventTitle= models.CharField(verbose_name= 'Event Title', max_length=100)
+    eventTitle= models.CharField(verbose_name= 'Event Title', max_length=100, null= False)
 
     EVENT_FREQ_PER_MONTH = [
         (0, 'One-off Event'),
@@ -32,14 +33,20 @@ class Event(models.Model):
     TYPE_CHOICES = [
         ('HH', 'Happy Hour'),
         ('SP', 'Student Special'),
-        ('HS', 'Holiday Special')
+        ('OT', 'Other'),
+        #('HS', 'Holiday Special')
     ]
-    type= models.CharField(verbose_name='Event Type', choices= TYPE_CHOICES, max_length=100)
+    type= models.CharField(verbose_name='Event Type', choices= TYPE_CHOICES, max_length=100, default= 'HH', null= True)
     #need to add price range
 
-    description= models.TextField(verbose_name='Event Description', max_length=2000)
+    description= models.TextField(verbose_name='Event Description', max_length=2000, null= True)
 
-    photos= models.ImageField()
+    time = models.CharField(verbose_name= 'Start and End time',  max_length=100, null= False)
+
+    # photos= models.ImageField()
+
+    def __str__(self):
+        return '{}'.format(self.eventTitle)
     
 
 
