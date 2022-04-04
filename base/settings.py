29b67319f7a462a93297daa7e49f803a858d1060
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2022-04-04 12:05:22 -0700

    gwg

--- base/settings.py
+++ base/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
 }
 }
 # postgres://ecjccqpliexgie:d6c389aa78a1d7799e9f480b0f0099fbe40ac1ccb01d75fc4d6b23ce28b6940b@ec2-52-205-3-3.compute-1.amazonaws.com:5432/d99678diphp5u6
-postgres://ecjccqpliexgie:d6c389aa78a1d7799e9f480b0f0099fbe40ac1ccb01d75fc4d6b23ce28b6940b@ec2-52-205-3-3.compute-1.amazonaws.com:5432/d99678diphp5u6
+
 # postgres://nyjgwguaismfkv:ad4fe38641f0eea146954691fac5678b715f41aa30813761e97b1a0a8aa8235f@ec2-3-218-47-9.compute-1.amazonaws.com:5432/dc2ro1vj5k090m
 
 
