8439e8dad8edfba5c99655ccc7dd882c08a2ca50
Reema <reema.a.shadid@gmail.com>
2022-04-04 21:17:42 -0700

    removed incorrect Event model registrations from comm app admin.py file

--- communication/admin.py
+++ communication/admin.py
@@ -3,13 +3,3 @@ from .models import *
 # Register your models here.
 admin.site.register(phoneSignup)
 
-class LocationInline(admin.StackedInline):
-    model = Location
-
-class EventAdmin(admin.ModelAdmin):
-    inlines = [
-        LocationInline,
-    ]
-
-admin.site.register(Event)
-admin.site.register(Location)
