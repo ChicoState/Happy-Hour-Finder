5985acf73b4e93e86743654f15ecb782f22ed299
Reema <reema.a.shadid@gmail.com>
2022-04-25 13:35:09 -0700

    created admin feature to mark Events active/inactive in bulk

--- Events/admin.py
+++ Events/admin.py
@@ -4,16 +4,31 @@ from .models import *
 
 # Register your models here.
 
+
+
+
 class LocationInline(admin.StackedInline):
     model = Location
 
+
 class EventAdmin(admin.ModelAdmin):
-    # inlines = [
-    #     LocationInline,
-    # ]
     list_filter = ('dayOfWeek', 'active','location__locationName')
     list_display = ('eventTitle','dayOfWeek','type','active')
+
+    actions = ['make_active', 'make_inactive']
+
+    @admin.action(description='Mark selected Events as Active')
+    def make_active(self, request, queryset):
+        queryset.update(active= True)
+
+    @admin.action(description='Mark selected Events as Inactive')
+    def make_inactive(self, request, queryset):
+        queryset.update(active= False)
+
     
+
+
+
 
 admin.site.register(Event, EventAdmin)
 admin.site.register(Location)
