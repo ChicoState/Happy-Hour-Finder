8482de53f91d39326583e58fed3937de7a430265
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2022-04-27 11:12:01 -0700

    added tabbing

--- communication/views.py
+++ communication/views.py
@@ -7,20 +7,28 @@ from django.views.generic import ListView
 # Create your views here.
 
 def home_view(request):
-    array = Location.objects.all()
-    paginator = Paginator(array, 50)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    allLocations = Location.objects.order_by('-locationName')
-    eventList=[]
-    for x in allLocations:
-        eventList.append(Event.objects.filter(location = x, active = True).order_by('-dayOfWeek'))
-    print(eventList)
-    for x in eventList:
-        print(x)
-    context = {
-        "location": page_obj,
+    locationData = []
+    location = Location.objects.all()
+    for local in location:
+        events = Event.objects.filter(location = local, active = True).order_by('-dayOfWeek')
+        data = {
+            "location": local,
+            "events": events
         }
+        locationData.append(data)
+
+    # paginator = Paginator(array, 50)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+    # allLocations = Location.objects.order_by('-locationName')
+    # eventList=[]
+    # for x in allLocations:
+    #     eventList.append(Event.objects.filter(location = x, active = True).order_by('-dayOfWeek'))
+    # print(eventList)
+    
+    context = {
+      "locationData" : locationData
+}
     return render(request, "index.html",context)
 
 class SearchResultsView(ListView):
